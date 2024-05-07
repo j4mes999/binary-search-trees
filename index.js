a1fb861966bb4ff97f53b460c0b7d373a2fac7ee
Luis J. Gonzalez <jamesqc999@gmail.com>
2024-05-07 07:57:59 -0500

    Add first implementation of delete node method

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 // const array = [7,5,3];
 let tree = createTree(array);
-//prettyPrint(tree.root);
- 
+
 tree.insert(6);
-// console.log(`index.js tree root ${tree.root.value}`);
-// console.log(`index.js tree root right ${tree.root.right.value}`);
-// console.log(`index.js tree root right left ${tree.root.right.left.value}`);
 tree.insert(10);
 prettyPrint(tree.root);
 
-//TODO continue building the methods mentioned in the description
+tree.deleteNode(4);
+prettyPrint(tree.root);
+
+
+//TODO Fix bug with delete node hard case
--- tree.js
+++ tree.js
@@ -24,6 +24,61 @@ function createTree(array){
         }
     }
 
+    const deleteNode = (value) => {
+        let current = root;
+        let parent = null;
+        while(current !== null && current.value !== value){
+            parent = current;
+            if(value < current.value){
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        if(current === null){
+            return;
+        }
+        if(current.left === null && current.right === null){
+            if(current.value < parent.value){
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        } else if(current.left === null){
+            if(current.value < parent.value){
+                parent.left = current.right;
+            } else {
+                parent.right = current.right;
+            }
+        } else if(current.right === null){
+            if(current.value < parent.value){
+                parent.left = current.left;
+            } else {
+                parent.right = current.left;
+            }
+        } else {
+            console.log(`tree.js current: ${current.value}`);
+            let rightSubTree = current.right;
+            let succesor = current;
+            while(rightSubTree.left !== null){
+                succesor = rightSubTree;
+                rightSubTree = rightSubTree.left;
+            }
+            succesor.left = null;
+            current.value = rightSubTree.value;
+            // while(rightSubTree.left !== null){
+            //     nodeTobeDeleted = rightSubTree;
+            //     rightSubTree = rightSubTree.left;
+            // }
+            // if(rightSubTree.value < nodeTobeDeleted.value){
+            //     nodeTobeDeleted.left = null;
+            // } else {
+            //     nodeTobeDeleted.right = null;
+            // }
+            // current.value = rightSubTree.value;
+        }
+    }
+
     function buildTree(array){
         let sortedArray = array.sort((a, b) => a - b);
         let uniqueArray = sortedArray.filter((value, index, self) => {
@@ -45,7 +100,7 @@ function createTree(array){
         return node;
     }
 
-    return {root, insert};
+    return {root, insert, deleteNode};
 }
 
 export default createTree;
